408d367ab8f97902c75b5fd9a7eae6c11c6ed047
Lucio Benfante <lucio@benfante.com>
2025-07-11 18:30:08 +0200

    refactor: remove deprecated user profile route and ensure proper role-based access for properties

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,14 +16,6 @@ export const routes: Routes = [
     data: { roles: ['USER'] },
   },
   {
-    path: 'pro',
-    title: 'YourRents - User Profile',
-    loadComponent: () => import('./profile/profile.component'),
-    canActivate: [AuthGuard],
-    data: { roles: ['USER'] },
-  },
-
-  {
     path: 'admin',
     title: 'YourRents - Admin',
     loadChildren: () => import('./admin/admin.routes').then((m) => m.routes),
@@ -35,10 +27,9 @@ export const routes: Routes = [
     path:"properties",
     title: 'YourRents - Properties',
     loadChildren: () => import('./properties/properties.routes').then((m) => m.routes),
-    canActivate: [AuthGuard],
+    canActivate: [canActivateAuthRole],
     data: { roles: ['USER'] },
   },
-
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: '403',
